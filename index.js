0c18f665f4bcdcf0ad63cb5a01f875a7a4d17841
skkyiee <srimithun2904@gmail.com>
2025-11-21 10:47:49 +0530

    new change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Your app is running via Jenkins and Docker using AWS [NEW APP].');
+  res.send('Hello World! Your app is running via Jenkins and Docker using AWS doing for viva.');
 });
 
 app.listen(PORT, () => {
